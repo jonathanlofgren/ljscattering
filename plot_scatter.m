e8692f705aac5189235c0ddb681e1579d9ab0273
Jonathan Lofgren <lofgren021@gmail.com>
2015-03-11 16:49:02 +0100

    Wrote a gaussian quadrature and made plots.

---
+++ plot_scatter.m
@@ -0,0 +1,29 @@
+close all
+clear all
+
+E_range = [0.1 1]; 
+
+E_vals = logspace(log10(E_range(1)), log10(E_range(2)), 5);
+
+% potential parameters
+sigma = 1;
+eps = 1;
+
+b_points = 1000;
+bvals = linspace(0, sigma*3, b_points);
+bvals = bvals(2:end-1);
+
+for E=E_vals
+    theta = zeros(1, b_points-2);
+    
+    for i=1:length(bvals)
+       theta(i) = scatter(E, bvals(i), sigma, eps); 
+    end
+   
+    plot(bvals, theta)
+    hold on
+end
+
+legend(num2str(E_vals(1)), num2str(E_vals(2)),num2str(E_vals(3)),num2str(E_vals(4)),num2str(E_vals(5)))
+xlabel('Impact parameter b')
+ylabel('Scattering angle (radians)')
--- scatter.m
+++ scatter.m
@@ -1,30 +1,20 @@
-clear all
+function theta = scatter(E, b, sigma, eps)
 
-E = 1000;
-b = 0.5;
+    % cutoff limit for the lj potential
+    r_max = 3*sigma;
 
-% parameters for the potential
-sigma = 1;
-eps = 1;
+    term1 = @(r) 1./r.^2 .* (1-(b./r).^2).^(-1/2);
+    term2 = @(r) 1./r.^2 .* (1-(b./r).^2 - lennard_jones(r,sigma,eps)/E).^(-1/2);
 
-r_max = 3*sigma;
-
-term1 = @(r) 1./r.^2 .* (1-(b./r).^2).^(-1/2);
-term2 = @(r) 1./r.^2 .* (1-(b./r).^2 - lennard_jones(r,sigma,eps)/E).^(-1/2);
-
-% find the outermost zero of the square root argument
-sqroot = @(r) 1-(b^2/r^2) - lennard_jones(r,sigma,eps)/E;
-outmost_zero = fzero(sqroot, 1);
-r_min = outmost_zero;
-
-x = linspace(0.9,10);
-y = zeros(length(x),1);
-for i=1:length(y)
-   y(i) = sqroot(x(i)); 
+    % find the outermost zero of the square root argument
+    sqroot = @(r) 1-(b^2/r^2) - lennard_jones(r,sigma,eps)/E;
+    outmost_zero = fzero(sqroot, 1);
+    r_min = outmost_zero;
+    %disp(['Found r_min = ' num2str(r_min)])
+    
+    n = 100;
+    b1 = gauss_quad(term1, b, r_max, n);
+    b2 = gauss_quad(term2, r_min, r_max, n);
+    
+    theta = b1-b2;  
 end
-plot(x,y)
-
-n = 10;
-a1 = integral(term1, b, r_max);
-a2 = integral(term2, r_min, r_max);
-a = a1-a2
